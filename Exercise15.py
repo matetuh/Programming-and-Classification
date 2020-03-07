8c185421574fd550b9ae574555a8e75df6a92c38
matetuh <matetuh@gmail.com>
2020-03-07 15:16:02 +0100

    Init Commit

---
+++ Exercise15.py
@@ -0,0 +1,6 @@
+# -------------- Exercise 15 ----------------------------------------------------
+#A list contains both integers and strings. Construct a function that sorts given list
+#in such a way that at the beginning of the resulting list one gets strings in alphabetical
+#order followed by integers in an increasing order.
+
+# -------------- Code  ---------------------------------------------------------
